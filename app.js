76bf8b35bd749d86ffdd1d2d48f7ccbbe045e34f
sep <sep.alamouti@sepalamouti.com>
2023-11-20 12:55:04 -0800

    javascript-initial-setup

---
+++ app.js
@@ -0,0 +1,8 @@
+const api_KEY = 'b456276f3f48ac6875be909e68351255'; // Replace with your actual API key
+const searchButton = document.getElementById('search-button');
+const cityInput = document.getElementById('city-input');
+const searchHistoryContainer = document.getElementById('search-history');
+const currentWeatherContainer = document.getElementById('current-weather');
+const forecastContainer = document.getElementById('forecast-container');
+const resetButton = document.getElementById('reset-button');
+
